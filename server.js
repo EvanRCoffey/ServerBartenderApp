1fd6dd016f1abdb1be0ef4c5b03223200351af54
REvanCof <rogerecoffey@gmail.com>
2017-02-09 19:51:06 -0600

    Added an alt.html, in case we need it

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var PORT = process.env.PORT || 8080;
 
 //ROUTING
 
-app.get("/whatever", function(req, res) {
-	res.sendFile(path.join(__dirname, "/../public/whatever.html"));
+app.get("/alt", function(req, res) {
+	res.sendFile(path.join(__dirname, "/./public/alt.html"));
 });
 
 // If no matching route is found default to home
@@ -21,9 +21,3 @@ app.use(function(req, res) {
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
-
-function myAmazingFunction(){
-	if(PORT === 5){
-		console.log('something')
-	}
-}
