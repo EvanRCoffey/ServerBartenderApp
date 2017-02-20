77eea98d768f308928621daaa46dbefbecde12a2
REvanCof <rogerecoffey@gmail.com>
2017-02-19 21:08:42 -0600

    Added my handlebars changes, got restaurants and jobs submitting correctly.

--- public/assets/javascript/job.js
+++ public/assets/javascript/job.js
@@ -10,23 +10,17 @@ $('.jobSubmitBtn').on('click', function(event) {
     event.preventDefault();
     console.log(event);
 
-	var restaurant_id = $("#restaurant_id").val().trim();
-    var user_id = $("#user_id").val().trim();
     var startDate = $("#startDate").val().trim();
     var endDate = $("#endDate").val().trim();
     var wage = $("#wage").val().trim();
-    var isReal = $("#isReal1").val().trim(); //Not working either
     //var userJobMenu = userJobMenu JSON location
     var comments = $("#comments").val().trim();
 
     //Create new user object.
     var newJob = {
-        restaurant_id:restaurant_id,
-        user_id:user_id,
         startDate:startDate,
         endDate:endDate,
         wage:wage,
-        isReal:isReal,
         //userJobMenu:userJobMenu,
         comments:comments
     };
--- public/assets/javascript/restaurant.js
+++ public/assets/javascript/restaurant.js
@@ -5,17 +5,13 @@ $('.restaurantSubmitBtn').on('click', function(event) {
     event.preventDefault();
     console.log(event);
 
-	var restaurant_id = $("#restaurant_id").val().trim();
     var restaurant_name = $("#restaurant_name").val().trim();
     //var defaultMenu = defaultMenu JSON location
-    var isReal = $("#isReal1").val().trim(); //Not working either
     
     //Create new user object.
     var newRestaurant = {
-        restaurant_id:restaurant_id,
         restaurant_name:restaurant_name,
         //defaultMenu:defaultMenu,
-        isReal:isReal,
     };
     // Send object via AJAX post
     $.post("/newRestaurant", newRestaurant)
