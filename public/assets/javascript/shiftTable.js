102600222df43fcc082f85d887cadeef102aab9b
mchogan7 <mills.c.hogan@gmail.com>
2017-02-27 22:23:36 -0600

    date slider working and tested

--- public/assets/javascript/shiftTable.js
+++ public/assets/javascript/shiftTable.js
@@ -1,71 +1,46 @@
 var totalShifts = $('tbody tr').length;
 var slider = document.getElementById('dateSlider');
-  noUiSlider.create(slider, {
-   start: [1, totalShifts],
-   connect: true,
-   step: 1,
-   range: {
-     'min': 1,
-     'max': totalShifts
-   },
-   format: wNumb({
-     decimals: 0
-   })
-  });
-  console.log($( "tbody tr:nth-child(12)"))
-  slider.noUiSlider.on('update', function(){
+noUiSlider.create(slider, {
+    start: [1, totalShifts],
+    connect: true,
+    step: 1,
+    range: {
+        'min': 1,
+        'max': totalShifts
+    },
+    format: wNumb({
+        decimals: 0
+    })
+});
+
+$("tbody tr").children().wrapInner('<div />')
+$("tbody tr").children().css('padding', '0')
+
+
+//Kill me.
+slider.noUiSlider.on('update', function() {
     var startDate = slider.noUiSlider.get()[0]
     var endDate = slider.noUiSlider.get()[1]
-    console.log(parseInt(endDate) )
+    console.log(parseInt(endDate))
     for (var i = 0; i < totalShifts + 1; i++) {
-      if(i < startDate || i > endDate){
-       $( "tbody tr:nth-child("+ i +")").removeClass('tableShow')
-       $( "tbody tr:nth-child("+ i +")").addClass('tableHide')
-      } else {
-      $( "tbody tr:nth-child("+ i +")").removeClass('tableHide')
-       $( "tbody tr:nth-child("+ i +")").addClass('tableShow')
-
-      }
-      
+        if (i < startDate || i > endDate) {
+            $("tbody tr:nth-child(" + i + ")").first().removeClass('tableShow')
+            $("tbody tr:nth-child(" + i + ")").closest('tr')
+                .children('td')
+                .animate({ padding: 0 })
+                .children()
+                .slideUp(function() { $(this).closest('tr').addClass('displayNone'); });
+        } else {
+            $("tbody tr:nth-child(" + i + ")").first().removeClass('displayNone')
+            $("tbody tr:nth-child(" + i + ")").first().addClass('tableShow').children('td')
+                .children().slideDown().css('padding', '15')
+        }
     }
-
-      })
-
-// for(var i=0; i<rowLength; i+=1){
-//   var row = table.rows[i];
-
-//   //your code goes here, looping over every row.
-//   //cells are accessed as easy
-
-//   var cellLength = row.cells.length;
-//   for(var y=0; y<cellLength; y+=1){
-//     var cell = row.cells[y];
-
-    //do something with every cell here
-  
-
-    
-
-    // if($( "tbody tr:nth-child("+ startDate +")" ).hasClass('tableHide')){
-    // $( "tbody tr:nth-child("+ (startDate - 1) +")" ).removeClass('tableHide')  
-    // $( "tbody tr:nth-child("+ startDate +")" ).addClass('tableShow')
-    // } else { $( "tbody tr:nth-child("+ startDate +")" ).addClass('tableHide')}
-   
-    // if($( "tbody tr:nth-child("+ endDate +")" ).hasClass('tableHide')){
-    // $( "tbody tr:nth-child("+ endDate +")" ).removeClass('tableHide')  
-    // $( "tbody tr:nth-child("+ endDate +")" ).addClass('tableShow')
-    // } else { $( "tbody tr:nth-child("+ endDate +")" ).addClass('tableHide')}
+    var startDateValue = $("tbody tr:nth-child(" + startDate + ")").find('td:first').text()
+    var endDateValue = $("tbody tr:nth-child(" + endDate + ")").find('td:first').text()
+    $('.date1').text(startDateValue)
+    $('.date2').text(endDateValue)
+})
 
 
 
-  
-
-//   var inputFormat = document.getElementById('input-format');
-
-// sliderFormat.noUiSlider.on('update', function( values, handle ) {
-//   inputFormat.value = values[handle];
-// });
-
-// inputFormat.addEventListener('change', function(){
-//   sliderFormat.noUiSlider.set(this.value);
-// });
