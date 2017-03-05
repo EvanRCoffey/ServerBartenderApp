a3e63bd700e8f022243b1d7e03f2734fef806eed
mchogan7 <mills.c.hogan@gmail.com>
2017-03-05 15:42:33 -0600

    bug fixing and shift table optimizations

--- public/assets/javascript/shiftTable.js
+++ public/assets/javascript/shiftTable.js
@@ -24,20 +24,32 @@ slider.noUiSlider.on('update', function() {
     for (var i = 0; i < totalShifts + 1; i++) {
         if (i < startDate || i > endDate) {
             $("tbody tr:nth-child(" + i + ")").first().removeClass('tableShow')
+            // $("tbody tr:nth-child(" + i + ")").find('td').addClass('displayNone')
             $("tbody tr:nth-child(" + i + ")").find('.autoRow').removeClass('tableShow')
             $("tbody tr:nth-child(" + i + ")").find('.autoRow').addClass('tableHide')
-            $("tbody tr:nth-child(" + i + ")").find('.shiftd').addClass('tableHide');
+            $("tbody tr:nth-child(" + i + ")").find('.shiftd').addClass('tableHide')
+                // .children('td')
+                // .animate({ padding: 0 })
+                // .children()
+                // .closest('tr').addClass('displayNone'); 
         } else {
             $("tbody tr:nth-child(" + i + ")").find('.autoRow').removeClass('tableHide')
+            $("tbody tr:nth-child(" + i + ")").find('.autoRow').removeClass('displayNone')
+            $("tbody tr:nth-child(" + i + ")").find('.shiftd').removeClass('displayNone')
+            $("tbody tr:nth-child(" + i + ")").find('.shiftd').removeClass('tableHide')
             // $("tbody tr:nth-child(" + i + ")").find('.autoRow').addClass('tableShow')
             // $("tbody tr:nth-child(" + i + ")").find('.shiftd').addClass('tableShow')
         }
     }
+
     var startDateValue = $("tbody tr:nth-child(" + startDate + ")").find('td:first').text()
     var endDateValue = $("tbody tr:nth-child(" + endDate + ")").find('td:first').text()
     $('.date1').text(startDateValue)
     $('.date2').text(endDateValue)
 })
 
+//     $('.autoRow').on('transitionend webkitTransitionEnd oTransitionEnd', function() {
+//    $('.tableHide').addClass('displayNone')
+// });
 
 
